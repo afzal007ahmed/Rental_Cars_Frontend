5d26295af6678011e94343a4992bd330ba183517
Afzal <afzalahmeddc@gmail.com>
2026-07-23 10:53:50 +0530

    added fix

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -284,7 +284,7 @@ const Bookings = () => {
                           </p>
 
                           <p className="text-sm text-muted-foreground">
-                            {booking.dropLocation?.city},{" "}
+                            {booking.dropLocation?.city}{" "}
                             {booking.dropLocation?.state}
                           </p>
                         </div>
